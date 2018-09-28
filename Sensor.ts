9e0306806d3980a4473bae53caaae2b0a9406d0a
Ruimin Huang <1447364060@qq.com>
2018-09-28 11:34:19 +0800

    add infrared proximity sensor drivr

--- Sensor.ts
+++ Sensor.ts
@@ -70,10 +70,10 @@ namespace Sound {
 
     /**
      * Set the Apin connected to Sound Sensor;
-     * @param a_pin_arg analog connected pin;
+     * @param a_pin_arg Sound Sensor analog connected pin;
      */
     //% blockId=Sound_setApin
-    //% block="analog pin |%a_pin_arg|"
+    //% block="sound sensor analog pin connect to |%a_pin_arg|"
     //% weight = 85
     export function setApin(a_pin_arg: AnalogPin): void {
         Apin = a_pin_arg;
@@ -82,18 +82,19 @@ namespace Sound {
 
     /**
      * Set the Dpin connected to Sound Sensor;
-     * @param d_pin_arg digital connected pin;
+     * @param d_pin_arg Sound Sensor digital connected pin;
      */
     //% blockId=Sound_setDpin
-    //% block="digital pin |%d_pin_arg|"
+    //% block="sound sensor digital pin connect to |%d_pin_arg|"
     //% weight = 75
     export function setDpin(d_pin_arg: DigitalPin): void {
         Dpin = d_pin_arg;
+        pins.setPull(Dpin,PinPullMode.PullUp);
     }
 
 
     /**
-     * Return the adc value from the AnalogPin;
+     * Return the sound sensor adc value from the AnalogPin;
      */
     //% blockId=Sound_getADCValue
     //% block="get sound adc value"
@@ -104,7 +105,7 @@ namespace Sound {
     }
 
     /**
-     * Return the volt value from the AnalogPin;
+     * Return the sound sensor volt value from the AnalogPin;
      */
     //% blockId=Sound_getVoltValue
     //% block="get sound volt value"
@@ -116,13 +117,100 @@ namespace Sound {
     }
 
     /**
-     * Return the volt value from the DigitalPin;
+     * Return the sound sensor volt value from the DigitalPin;
      */
     //% blockId=Sound_getNoise
-    //% block="get whether noise"
+    //% block="get whether noise is loud"
     //% weight = 45
     export function getNoise(): number {
         noise = pins.digitalReadPin(Dpin);
-        return noise;
+        if (noise) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
+
+
+
+
+
+
+/*
+infrared  block
+*/
+//% weight=40 color=#FF0000 icon="\uf0eb" block="infrared"
+namespace Infrared {
+    let Apin = AnalogPin.P0;
+    let Dpin = DigitalPin.P1;
+
+    let close = 0;
+    let volt = 0;
+    let adc = 0;
+
+
+    /**
+     * Set the Apin connected to Infrared Sensor;
+     * @param a_pin_arg Infrared Sensor analog connected pin;
+     */
+    //% blockId=Infrared_setApin
+    //% block="infrared sensor analog pin connect to |%a_pin_arg|"
+    //% weight = 85
+    export function setApin(a_pin_arg: AnalogPin): void {
+        Apin = a_pin_arg;
+    }
+
+
+    /**
+     * Set the Dpin connected to Infrared Sensor;
+     * @param d_pin_arg Infrared Sensor digital connected pin;
+     */
+    //% blockId=Infrared_setDpin
+    //% block="infrared sensor digital pin connect to |%d_pin_arg|"
+    //% weight = 75
+    export function setDpin(d_pin_arg: DigitalPin): void {
+        Dpin = d_pin_arg;
+        pins.setPull(Dpin,PinPullMode.PullUp);
+    }
+
+
+    /**
+     * Return the infrared sensor adc value from the AnalogPin;
+     */
+    //% blockId=Infrared_getADCValue
+    //% block="get infrared adc value"
+    //% weight = 65
+    export function getADCValue(): number {
+        adc = pins.analogReadPin(Apin);
+        return adc;
+    }
+
+    /**
+     * Return the infrared sensor volt value from the AnalogPin;
+     */
+    //% blockId=Infrared_getVoltValue
+    //% block="get infrared volt value"
+    //% weight = 55
+    export function getVoltValue(): number {
+        adc = pins.analogReadPin(Apin);
+        volt = adc * 3300 / 1024;
+        return volt;
+    }
+
+    /**
+     * Return the infrared sensor volt value from the DigitalPin;
+     */
+    //% blockId=Infrared_getClose
+    //% block="get whether infrared near obstacles"
+    //% weight = 45
+    export function getClose(): number {
+        close = pins.digitalReadPin(Dpin);
+        if (close) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
+
--- test.ts
+++ test.ts
@@ -1,5 +1,36 @@
 // tests go here; this will not be compiled when this package is used as a library
 
+
+/*
+let volt = 0
+
+let adc = 0
+
+basic.showIcon(IconNames.Heart)
+
+serial.redirectToUSB()
+
+Liquid.setPin(AnalogPin.P0)
+
+basic.forever(() => {
+
+    adc = Liquid.getADCValue()
+
+    serial.writeValue("adc", adc)
+
+    volt = Liquid.getVoltValue()
+
+    serial.writeValue("mV", volt)
+
+    basic.pause(500)
+})
+*/
+
+
+
+
+
+/*
 let noise = 0
 
 let volt = 0
@@ -27,6 +58,42 @@ basic.forever(() => {
     noise = Sound.getNoise()
 
     serial.writeValue("noise", noise)
-    
+
     basic.pause(100)
 })
+*/
+
+
+
+
+let close = 0
+
+let volt = 0
+
+let adc = 0
+
+Infrared.setApin(AnalogPin.P0)
+
+Infrared.setDpin(DigitalPin.P1)
+
+basic.showIcon(IconNames.Heart)
+
+serial.redirectToUSB()
+
+basic.forever(() => {
+
+    adc = Infrared.getADCValue()
+
+    serial.writeValue("adc", adc)
+
+    volt = Infrared.getVoltValue()
+
+    serial.writeValue("mV", volt)
+
+    close = Infrared.getClose()
+
+    serial.writeValue("close", close)
+
+    basic.pause(100)
+})
+
